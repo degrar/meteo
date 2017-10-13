441e232da85b175cad0aaa70141965980cf784a1
Gerard Alba <gerard.a.c@gmail.com>
2017-10-13 13:17:17 +0200

    change gitignore

--- laravel/config/morillas.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'mail' => 'albert.horta@morillas.com',
-    'mail_user' => 'Albert Horta Llobet',
-    'google_map_key' => 'AIzaSyAs4K_d6Oil0qYtwSN2OFuJhor_lt43HBQ',
-];
--- laravel/resources/views/widgets/footer.blade.php
+++
@@ -1,5 +0,0 @@
-@include('widgets.content.footer')
-{{-- Inject main.js on pre_body_closed : This js contains all the js logic that belongs to the main template --}}
-@push('pre_body_closed')
-	{{ Html::script('js/main.js') }}
-@endpush
